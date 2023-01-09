b95482882ee4bde25e71d74341bca58aeb0293f8
simonrichard-dev <simon.insa@gmail.com>
2023-01-09 15:29:14 +0100

    1-add binary_tree_insert_left for task 1 and update binary_trees.h adding prototype

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert a node as a left-child
+ * @parent: the parent ptr
+ * @value: value of each node
+ *
+ * Return: the new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node_l;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node_l = malloc(sizeof(binary_tree_t));
+	if (new_node_l == NULL)
+		return (NULL);
+	new_node_l->n = value;
+	new_node_l->parent = parent;
+	new_node_l->left = NULL;
+	new_node_l->right = NULL;
+	if (parent->left != NULL)
+	{
+		new_node_l->left = parent->left;
+		new_node_l->left->parent = new_node_l;
+	}
+	parent->left = new_node_l;
+
+        return (new_node_l);
+}
