2115755ac4d944a6b03aa09eb9f29549efc8ded3
Ethan1709 <benyayerethan@gmail.com>
2023-01-09 08:13:19 -0800

    updating file 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
- * @tree: ptr to the node to check
+ *node: ptr to the node to check
  *
  * Return: 1 if a leaf or 0
  */
@@ -14,7 +14,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 
 	if (node->left == NULL && node->right == NULL)
 		return (1);
-	else 
+	else
 		return (0);
 	return (0);
 }
