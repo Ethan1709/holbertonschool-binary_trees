e5667276a104e7c80711230d2fd679496c6bdc2c
simonrichard-dev <simon.insa@gmail.com>
2023-01-10 14:47:46 +0100

    add 8-binary_tree_postorder.c file

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - goes through a binary tree post-order traversal
+ * @tree: ptr on the binary tree
+ * @func: ptr to a function to call for each node
+ *
+ * Return: 1 if a leaf or 0
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
