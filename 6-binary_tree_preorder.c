e7bed3ddc9ea97326a58636812371449bb7fd571
Ethan1709 <benyayerethan@gmail.com>
2023-01-10 05:14:36 -0800

    adding file 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree in preo-order traversal
+ * @tree: ptr on the binary tree
+ * @func: ptr to a function to call for each node
+ *
+ * Return: 1 if a leaf or 0
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
