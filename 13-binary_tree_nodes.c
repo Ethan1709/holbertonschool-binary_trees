9fe393ec76a9c83a12db8221f318847efaa4b3b7
Ethan1709 <benyayerethan@gmail.com>
2023-01-11 04:24:11 -0800

    adding file 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,8 +12,13 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->right != NULL || tree->left != NULL)
-		return (1);
+	if (tree->right != NULL && tree->left != NULL)
+		return (1 + (binary_tree_nodes(tree->right) +
+		binary_tree_nodes(tree->left)));
+	if (tree->right != NULL)
+		return (binary_tree_nodes(tree->right) + 1);
+	if (tree->left != NULL)
+		return (binary_tree_nodes(tree->left) + 1);
 
 	return (binary_tree_nodes(tree->left) +
 		binary_tree_nodes(tree->right));
