3b03f409e7105636f6c89a5ec3209d9d48348e95
Ethan1709 <benyayerethan@gmail.com>
2023-01-11 01:37:04 -0800

    fixing file 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!=tree->right && !=tree->left)
+	if (tree->right != NULL && tree->left != NULL)
 		return (1);
 
 	return (binary_tree_leaves(tree->left) +
